e6c404f5be6ae1a5d289a2dfaed17e6f5a72f01d
aalvaroo7 <almarrom@myuax.com>
2023-11-03 10:20:59 +0100

    ejercicios 2 y 6 (alvaro)

--- funcionerror(ej2).cpp
+++ funcionerror(ej2).cpp
@@ -5,8 +5,6 @@
 #include <cmath>
 
 using namespace std;
-
-//version de la funcion que devuelve un valor de error en este caso la funcion de la division entre cero
 double division(double x, double y){
     if (y==0.0)
     return 0.0; //devuelve 0.0 para indicar un error
@@ -26,7 +24,7 @@ if (y==0.0) {
 cout<<"el resultado es "<<resultado<<endl;
 return 0;
 }
-//version de la funcion que devuelve un valor de error en este caso la funcion de la division entre cero
+
   
 
 
--- funcionerrorquedevuelveunnumeroencasodeerror(ej2).cpp
+++ funcionerrorquedevuelveunnumeroencasodeerror(ej2).cpp
@@ -1,14 +1,5 @@
 
-//para esta funcion emplearemos el codigo usado en funcionerror.cpp solo que en este caso
-//la funcion no devolvera error: division entre cero
-//sino que devolvera un numero en caso de error
-//para ello emplearemos la funcion isnan() que devuelve un valor booleano
-//en caso de que el numero sea un nan
-//y en caso de que no sea un nan devolvera el resultado de la division
-//en caso de que sea un nan devolvera un numero en caso de error
-//en este caso devolvera 1.0
-//la funcion isnan() se encuentra en la libreria cmath
-//para emplearla debemos incluir la libreria cmath
+
 #include <iostream>
 #include <iomanip>
 #include <string>
